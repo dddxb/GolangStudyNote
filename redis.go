34b0618b0b20e4089c511a2f99a2498afde9a5ec
panco <panco0825>
2019-01-08 12:01:07 +0800

    update

--- redis.go
+++ redis.go
@@ -21,10 +21,14 @@ func connect() {
 func main() {
 	connect()
 	err := r.Set("a", "a", 0).Err()
+	r.HMSet("player1", map[string]interface{}{
+		"name": "panco",
+		"age":  23,
+	})
 	Err(err)
-	val, err := r.Get("a").Result()
-	Err(err)
-	fmt.Println(val)
+	cmd, err2 := r.HMGet("player1", "name", "age").Result()
+	Err(err2)
+	fmt.Println(cmd[0])
 }
 
 func Err(err error) {
